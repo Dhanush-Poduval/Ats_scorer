499ba5a9339008b1f3b89c1dfc5d2f6d67c4a9fa
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-17 00:04:53 +0530

    feat:added the job descreption endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI,UploadFile,File
+from models import Job
 import PyPDF2
 import io
 app=FastAPI()
 
-def extract_pdf(file:UploadFile):
+def extract_pdf(file:io.BytesIO):
     pdf_reader=PyPDF2.PdfReader(file)
     text=""
     for page in pdf_reader.pages:
@@ -18,3 +19,11 @@ async def upload_resume(file:UploadFile=File(...)):
     text=extract_pdf(pdf_file)
 
     return {'resume_text':text[:500]}
+
+@app.post('/job_desc')
+def job_desc(job:Job):
+    job_text=job.text
+    return job_text
+    
+
+
