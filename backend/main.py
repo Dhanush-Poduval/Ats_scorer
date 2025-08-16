7ef824d2414d03f436ffcc235d449b54e5a1f47f
Dhanush-Poduval <dhanushpoduval@gmail.com>
2025-08-16 23:42:15 +0530

    feat: made the pdf reading part

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,UploadFile,File
+import PyPDF2
+import io
 app=FastAPI()
 
-@app.get('/test')
-def test():
-    return ("working properly")
+def extract_pdf(file:UploadFile):
+    pdf_reader=PyPDF2.PdfReader(file)
+    text=""
+    for page in pdf_reader.pages:
+        text+=page.extract_text()or""
+    return text
+
+@app.post('/uploade_resume')
+async def upload_resume(file:UploadFile=File(...)):
+    file_content=await file.read()
+    pdf_file=io.BytesIO(file_content)
+
+    text=extract_pdf(pdf_file)
+
+    return {'resume_text':text[:500]}
